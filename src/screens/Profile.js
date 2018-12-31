f4fc3ccd9e65ee94c429216138a991c60e8f4be1
DESKTOP-MSG9PH7\Shivam <shivam.gupta@varaunited.com>
2018-12-31 11:53:19 +0530

    UI Changes to Profile

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, TouchableOpacity, StyleSheet, Dimensions} from 'react-native';
 import firebase from 'react-native-firebase';
 import AddDetails from './auth/AddDetails';
 
+const { width: WIDTH } = Dimensions.get('window')
+
 class ProfileScreen extends Component{
+
 
   handleSignout = () => {
       firebase
@@ -14,14 +17,60 @@ class ProfileScreen extends Component{
 
   render(){
     return(
-      <View>
-        <Text>{firebase.auth().currentUser.uid}</Text>
-        <Text>Profile Screen</Text>
+      <View style={styles.backgroundContainer}>
+        {/* <Text>{firebase.auth().currentUser.uid}</Text> */}
+        <View style={styles.headerContainer}>
+        <Text style={styles.titleText}>Profile</Text>
+        <Text style={styles.subtitleText}>What can you offer?</Text>
+        </View>
         <AddDetails />
-        <Button title="Sign Out" onPress={() => firebase.auth().signOut()} />
+
+        <TouchableOpacity style={styles.btn} onPress={() => firebase.auth().signOut()}>
+          <Text style={styles.btnText}> Signout </Text>
+        </TouchableOpacity>
+
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  backgroundContainer: {
+    flex: 1,
+    paddingLeft: 15,
+    paddingRight: 8
+  },
+  btn: {
+    width: WIDTH - 20,
+    height: 45,
+    borderRadius: 25,
+    backgroundColor: 'darkgrey',
+    justifyContent: 'center',
+    marginTop: 20
+  },
+  titleText: {
+    color: 'rgba(0, 0, 0, 0.8)',
+    fontSize: 28,
+    fontWeight: '500',
+    textAlign: 'left'
+  },
+  subtitleText: {
+    color: 'rgba(0, 0, 0, 0.8)',
+    fontSize: 20,
+    fontWeight: '200',
+    textAlign: 'left'
+  },
+  headerContainer: {
+    paddingLeft: 10,
+    marginTop: 20,
+    marginBottom: 15
+  },
+  btnText: {
+    color: 'rgba(255, 255, 255, 1)',
+    fontSize: 16,
+    textAlign: 'center'
+  }
+})
+
 
 export {ProfileScreen};

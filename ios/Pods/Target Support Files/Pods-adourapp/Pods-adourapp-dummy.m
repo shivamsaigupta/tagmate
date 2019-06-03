9629ca8fec1ef81fbe2f8aacf20f50da00df5ca7
user153892 <user153892@MU207.local>
2019-06-03 12:27:19 +0530

    Renaming app adourapp to chillmate

--- ios/Pods/Target Support Files/Pods-adourapp/Pods-adourapp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_adourapp : NSObject
-@end
-@implementation PodsDummy_Pods_adourapp
-@end
---
+++ ios/Pods/Target Support Files/Pods-chillmate/Pods-chillmate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_chillmate : NSObject
+@end
+@implementation PodsDummy_Pods_chillmate
+@end

dad2cab603001b4525421f9246d667d64a5d42e4
DESKTOP-MSG9PH7\Shivam <shivam@shivam.me>
2019-07-19 17:37:03 +0530

    Google Play 1.51 Release

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -96,7 +96,7 @@ class ProfileScreen extends Component{
   loadWhatsapp = () =>
   {
     // Triggering the app to open whatsapp with a preloaded message.
-    Linking.openURL('whatsapp://send?text=Hey, checkout Instajude. I use it know what events are happening on campus - http://instajude.com')
+    Linking.openURL('whatsapp://send?text=Hey, checkout Instajude. I use it know what\'s happening on campus - https://instajude.com')
   }
 
   /**
